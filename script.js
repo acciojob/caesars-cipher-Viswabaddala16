b1762fd217c9bd989b9202902e37cf62e2187216
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-04-10 12:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Your Script here.
+function rot13(str) {
+	
+	let result = "";
+	
+	for (let i = 0; i < str.length; i++) {
+		
+		let ascii = str.charCodeAt(i);
+	    if (ascii >= 65 && ascii <= 90) {
+			
+			 
+	        ascii = ((ascii - 65 + 13) % 26) + 65;
+		}
+	}	
+	result += String.fromCharCode(ascii);
+}
+return result;
+
+
+		
+		
+		
+	
+	
